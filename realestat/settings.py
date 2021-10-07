cb9620643c0cc41a33b61c72fd39d51ae7086ebe
Kwami Sitou <kudawoo2002@gmail.com>
2021-10-07 14:32:59 +0000

    new commit

--- realestat/settings.py
+++ realestat/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static_cdn'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'realestat/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_URL = '/meida/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -131,3 +131,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
