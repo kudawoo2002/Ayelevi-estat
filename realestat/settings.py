75dfa0528bc2eb8d4bfd2258aa81fa8f6de45529
Kwami Sitou <kudawoo2002@gmail.com>
2021-10-07 14:52:07 +0000

    make it better

--- realestat/settings.py
+++ realestat/settings.py
@@ -131,4 +131,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
